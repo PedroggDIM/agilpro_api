bbcb5b16695209ce2a14dcd0fbad53927645c06b
Pedro Gil <pedrogg76@yahoo.es>
2023-10-06 18:22:49 +0200

    4. Componente formulario para la creación de una nueva incidencia. fix #4

---
+++ frontend/src/components/Incidencias/GestionarIncidenciasSABAS.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      titulo: 'Esta es la pantalla para Gestionar Incidencias por el SABAS',
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+  </div>
+</template>
